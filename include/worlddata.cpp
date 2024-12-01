aa427d70f6967b242ca4d8c2ca971a91159fa0de
miguell <mihailomijacic@gmail.com>
2024-12-01 19:03:48 +0100

    worlddata stuff

--- include/worlddata.cpp
+++ include/worlddata.cpp
@@ -7,21 +7,23 @@
 void getWorldData(){
     std::string dir = "world/" + world::worldName + ".dat";
     std::ifstream data(dir);
-    if (data.bad()){
+    if (!data.bad()){
+        getline(data, world::worldName);
+        data >> world::worldTime.day;
+        data >> world::worldTime.hour;
+        for (int i = 0; i<26; i++){
+            data >> world::dangerLevel[i];
+        }
+        for (int i = 0; i<100; i++){
+            getline(data, world::itemlist[i].name);
+            getline(data, world::itemlist[i].effect);
+            data >> world::itemlist[i].id;
+        }
+        LogThis("retrieved %s.dat",player::username.c_str());
+    }
+    else {
         LogThis("bad data location");
     }
-    getline(data, world::worldName);
-    data >> world::worldTime.day;
-    data >> world::worldTime.hour;
-    for (int i = 0; i<26; i++){
-        data >> world::dangerLevel[i];
-    }
-    for (int i = 0; i<100; i++){
-        getline(data, world::itemlist[i].name);
-        getline(data, world::itemlist[i].effect);
-        data >> world::itemlist[i].id;
-    }
-    LogThis("retrieved %s.dat",player::username.c_str());
 }
 void writeWorldData(){
     std::string dir = "world/" + player::username + ".dat";
